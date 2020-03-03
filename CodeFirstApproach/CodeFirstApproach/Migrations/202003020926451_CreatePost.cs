cc4c6077e67dee409afd43e4be4a4942bd567b85
ShamsiShakeeb <jakas1gy@gmail.com>
2020-03-03 08:24:37 +0600

    first commit

---
+++ CodeFirstApproach/CodeFirstApproach/Migrations/202003020926451_CreatePost.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeFirstApproach.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Information",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Information");
+        }
+    }
+}
---
+++ CodeFirstApproach/CodeFirstApproach/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproach
+{
+    class Program
+    {
+
+        public class Information
+        {
+            public int id { set; get; }
+            public String Name { set; get; }
+            public String Email { set; get; }
+
+            public String Password { set; get; }
+        }
+
+        public class DB: DbContext
+        {
+            public DbSet<Information> InfoTable { set; get; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter ID: ");
+            int ID = Int32.Parse(Console.ReadLine().ToString());
+            Console.WriteLine("Enter Name: ");
+            String NAME = Console.ReadLine();
+            Console.WriteLine("Enter Email: ");
+            String EMAIL = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            String PASSWORD = Console.ReadLine();
+
+            DB context = new DB();
+            var post = new Information()
+            {
+                id = ID,
+                Name = NAME,
+                Email = EMAIL,
+                Password = PASSWORD,
+
+            };
+            context.InfoTable.Add(post);
+            context.SaveChanges();
+
+            var select = context.InfoTable.Select(x => new { x.id, x.Name, x.Email }).ToList();
+            for(int i = 0; i < select.Count; i++)
+            {
+                Console.WriteLine(select[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
